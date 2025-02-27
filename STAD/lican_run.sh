932a617d679bdb9b57c19573813ed56f92aa6f5a
zhuge-c <zhuge-c@users.noreply.github.com>
2025-02-27 22:45:24 +0800

    modification

--- STAD/lican_run.sh
+++
@@ -1,5 +0,0 @@
-current_time_star=$(date "+%Y-%m-%d %H:%M:%S")
-echo "运行时时间: $current_time_star"
-python3 run_cbba_example_01.py
-current_time_end=$(date "+%Y-%m-%d %H:%M:%S")
-echo "运行后时间: $current_time_end"
